99864a99eed624a9a1a4519bd5f17a02497b83dd
Luca Heltai <luca.heltai@unipi.it>
2026-03-20 08:08:00 +0100

    Indent

--- codes/dealii/execs/laplacian.cc
+++ codes/dealii/execs/laplacian.cc
@@ -5,12 +5,12 @@
 // This file is part of the deal.II Testbench for NMOPT Laboratories,
 // deal.II library.
 //
-// The deal.II Testbench for NMOPT Laboratories is free software; you can use it,
-// redistribute it, and/or modify it under the terms of the Apache-2.0 License
-// WITH LLVM-exception as published by the Free Software Foundation; either
-// version 3.0 of the License, or (at your option) any later version.
-// The full text of the license can be found in the file LICENSE.md
-// at the top level of the deal.II Testbench for NMOPT Laboratories distribution.
+// The deal.II Testbench for NMOPT Laboratories is free software; you can use
+// it, redistribute it, and/or modify it under the terms of the Apache-2.0
+// License WITH LLVM-exception as published by the Free Software Foundation;
+// either version 3.0 of the License, or (at your option) any later version. The
+// full text of the license can be found in the file LICENSE.md at the top level
+// of the deal.II Testbench for NMOPT Laboratories distribution.
 //
 // ---------------------------------------------------------------------
 #include "laplacian.h"
